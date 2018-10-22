5b0dd28567b5d574ddb87360b2249e99b233ecfa
gconcepcion-csueb <gconcepcion2@horizon.csueastbay.edu>
2018-10-22 16:01:02 -0700

    Added GameTest and PlayerTest

---
+++ test/tests/GameTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import game.Game;
+
+public class GameTest {
+    
+    private Game game;
+    
+    public GameTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
---
+++ test/tests/PlayerTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import game.Player;
+
+/**
+ *
+ * @author gabri
+ */
+public class PlayerTest {
+    
+    private Player player;
+    
+    public PlayerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
